7d8d2fb50c198e2ebc2137a2fef9100b5d6af333
Masaki Waga <masakiwaga@gmail.com>
2025-03-19 09:22:11 +0100

    Added gtime_to_json.sh

---
+++ utils/gtime_to_json.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+################################################################
+# Name
+#  gtime_to_json.sh 
+#
+# Description
+#  Generate a JSON file from the logs of GNU time.
+#
+# Example
+#  ./gtime_to_json.sh
+#
+# Author
+#  Masaki Waga
+#
+# License
+#  MIT License
+################################################################
+
+# Move to the 
+cd "$(dirname "$0")" || exit 1
