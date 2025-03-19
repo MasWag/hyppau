b24009c2d7a37647f9ae818cac340cd9adf63818
Masaki Waga <masakiwaga@gmail.com>
2025-03-19 13:45:39 +0100

    Added utilities for analysis

--- utils/gtime_to_json.sh
+++ utils/gtime_to_json.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ################################################################
 # Name
-#  gtime_to_json.sh 
+#  gtime_to_json.sh
 #
 # Description
 #  Generate a JSON file from the logs of GNU time.
@@ -18,3 +18,9 @@
 
 # Move to the 
 cd "$(dirname "$0")" || exit 1
+
+for file in ../logs/*.gtime.log; do
+    jc --time < "$file" |
+        # extract 2025[0-9]\+-[0-9]\+ from .command_being_timed and use it as "id"
+        jq -r '. + {id: (.command_being_timed | match("2025[0-9]+-[0-9]+").string)}'
+done | jq -s .
---
+++ utils/stderr_to_json.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -ue
+################################################################
+# Name
+#  stderr_to_json.sh
+#
+# Description
+#  Generate a JSON file from the logs of HypPAu.
+#
+# Prerequisites
+#  - jo (https://jpmens.net/2016/03/05/a-shell-command-to-create-json-jo/)
+#    - You can install jo with brew install jo on macOS (with Homebrew)
+#  - jq
+#
+# Example
+#  ./stderr_to_json.sh
+#
+# Author
+#  Masaki Waga
+#
+# License
+#  MIT License
+################################################################
+
+cd "$(dirname "$0")" || exit 1
+
+for file in ../logs/*.stderr.log; do
+    benchmark=$(basename "$file" | cut -d - -f 1)
+    id=$(basename "$file" | grep -o '2025[0-9]\+-[0-9]\+')
+    case $benchmark in
+        network_pair)
+            length=$(basename "$file" | cut -d - -f 2)
+            mode=$(basename "$file" | cut -d - -f 3)
+            ;;
+        *)
+            length=$(basename "$file" | cut -d - -f 3)
+            mode=$(basename "$file" | cut -d - -f 4)
+    esac
+    if [ "$mode" = fjs ] || [ "$mode" = fjs ]; then
+        kmp_time=$(grep kmp "$file" | tr -d ')' | sed 's/^.* //;')
+        qs_time=$(grep quick "$file" | tr -d ')' | sed 's/^.* //;')
+    else
+        kmp_time=""
+        qs_time=""
+    fi
+    if [ "$benchmark" = dimensions ]; then
+        dimension=$(basename "$file" | cut -d - -f 2)
+        jo -p id="$id" benchmark="$benchmark" length="$length" mode="$mode" dimension="$dimension" kmp_time="$kmp_time" qs_time="$qs_time"
+    elif [ "$benchmark" != network_pair ]; then
+        alphabet=$(basename "$file" | cut -d - -f 2)
+        jo -p id="$id" benchmark="$benchmark" length="$length" mode="$mode" alphabet="$alphabet" kmp_time="$kmp_time" qs_time="$qs_time"
+    else
+        jo -p id="$id" benchmark="$benchmark" length="$length" mode="$mode" kmp_time="$kmp_time" qs_time="$qs_time"
+    fi
+done | jq -s .
